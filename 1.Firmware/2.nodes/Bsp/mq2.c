d2a97407d89fc701e4e78ea4fa2a9ff6841aafaf
ErBW_s <erbws@foxmail.com>
2025-03-15 00:14:10 +0800

    blank mq2 files

---
+++ 1.Firmware/2.nodes/Bsp/mq2.c
@@ -0,0 +1,8 @@
+/*!
+ * Copyright (c) 2025, ErBW_s
+ * All rights reserved.
+ * 
+ * @author  Baohan
+ */
+
+#include "mq2.h"
---
+++ 1.Firmware/2.nodes/Bsp/mq2.h
@@ -0,0 +1,21 @@
+/*!
+ * Copyright (c) 2025, ErBW_s
+ * All rights reserved.
+ * 
+ * @author  Baohan
+ */
+
+#ifndef NODES_MQ2_H
+#define NODES_MQ2_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
