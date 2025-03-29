840bdd9accc43233cb15a16687fd119d0aae52dc
ErBW_s <erbws@foxmail.com>
2025-03-29 23:32:33 +0800

    fix: uart channel

--- 1.Firmware/2.nodes/Bsp/lora/lora.c
+++ 1.Firmware/2.nodes/Bsp/lora/lora.c
@@ -13,7 +13,7 @@ static uint8_t gateway_frame[3] = {0x00, 0x01, 0x0A};
 
 void lora_send_to_gateway(const uint8_t addr, float val)
 {
-    HAL_UART_Transmit(&huart1, gateway_frame, 3, 0xFFFF);
-    HAL_UART_Transmit(&huart1, &addr, 1, 0xFFFF);
-    HAL_UART_Transmit(&huart1, (uint8_t *) &val, 4, 0xFFFF);
+    HAL_UART_Transmit(&huart2, gateway_frame, 3, 0xFFFF);
+    HAL_UART_Transmit(&huart2, &addr, 1, 0xFFFF);
+    HAL_UART_Transmit(&huart2, (uint8_t *) &val, 4, 0xFFFF);
 }
