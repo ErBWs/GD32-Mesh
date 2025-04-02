5a64bfc0dbe70e8b1a2b6f53b53aaa6472ad84c9
ErBW_s <erbws@foxmail.com>
2025-04-03 01:12:38 +0800

    remove some useless code

--- 1.Firmware/3.4g/single_mqtt.lua
+++ 1.Firmware/3.4g/single_mqtt.lua
@@ -3,12 +3,12 @@
 -- pm.request(pm.LIGHT)
 
 --根据自己的服务器修改以下参数
-local mqtt_host = "lbsmqtt.airm2m.com"
-local mqtt_port = 1884
+local mqtt_host = "0c5e592236.st1.iotda-device.cn-east-3.myhuaweicloud.com"
+local mqtt_port = 8883
 local mqtt_isssl = false
 local client_id = "abc"
 local user_name = "user"
-local password = "password"
+local password = ""
 
 local pub_topic = "/luatos/pub/" .. (mcu.unique_id():toHex())
 local sub_topic = "/luatos/sub/" .. (mcu.unique_id():toHex())
@@ -20,42 +20,7 @@ local mqttc = nil
 -- 统一联网函数
 sys.taskInit(function()
     local device_id = mcu.unique_id():toHex()
-    -----------------------------
-    -- 统一联网函数, 可自行删减
-    ----------------------------
-    if wlan and wlan.connect then
-        -- wifi 联网, ESP32系列均支持
-        local ssid = "luatos1234"
-        local password = "12341234"
-        log.info("wifi", ssid, password)
-        -- TODO 改成自动配网
-        -- LED = gpio.setup(12, 0, gpio.PULLUP)
-        wlan.init()
-        wlan.setMode(wlan.STATION) -- 默认也是这个模式,不调用也可以
-        device_id = wlan.getMac()
-        wlan.connect(ssid, password, 1)
-    elseif mobile then
-        -- Air780E/Air600E系列
-        --mobile.simid(2) -- 自动切换SIM卡
-        -- LED = gpio.setup(27, 0, gpio.PULLUP)
-        device_id = mobile.imei()
-    elseif w5500 then
-        -- w5500 以太网, 当前仅Air105支持
-        w5500.init(spi.HSPI_0, 24000000, pin.PC14, pin.PC01, pin.PC00)
-        w5500.config() --默认是DHCP模式
-        w5500.bind(socket.ETH0)
-        -- LED = gpio.setup(62, 0, gpio.PULLUP)
-    elseif socket or mqtt then
-        -- 适配的socket库也OK
-        -- 没有其他操作, 单纯给个注释说明
-    else
-        -- 其他不认识的bsp, 循环提示一下吧
-        while 1 do
-            sys.wait(1000)
-            log.info("bsp", "本bsp可能未适配网络层, 请查证")
-        end
-    end
-    -- 默认都等到联网成功
+    device_id = mobile.imei()
     sys.waitUntil("IP_READY")
     sys.publish("net_ready", device_id)
 end)
@@ -74,17 +39,6 @@ sys.taskInit(function()
     -- 可使用mqtt.x等客户端进行调试
     log.info("mqtt", "pub", pub_topic)
     log.info("mqtt", "sub", sub_topic)
-
-    -- 打印一下支持的加密套件, 通常来说, 固件已包含常见的99%的加密套件
-    -- if crypto.cipher_suites then
-    --     log.info("cipher", "suites", json.encode(crypto.cipher_suites()))
-    -- end
-    if mqtt == nil then
-        while 1 do
-            sys.wait(1000)
-            log.info("bsp", "本bsp未适配mqtt库, 请查证")
-        end
-    end
 
     -------------------------------------
     -------- MQTT 演示代码 --------------
@@ -148,30 +102,17 @@ sys.taskInit(function()
     end
 end)
 
--- 以下是演示与uart结合, 简单的mqtt-uart透传实现,不需要就注释掉
 local uart_id = 2
 uart.setup(uart_id, 115200)
 uart.on(uart_id, "receive", function(id, len)
     local data = ""
-    while 1 do
+    while true do
         local tmp = uart.read(uart_id)
         if not tmp or #tmp == 0 then
             break
         end
         data = data .. tmp
     end
-    log.info("uart", "uart收到数据", data)
+    log.info("uart", "\r\n", data)
     sys.publish("mqtt_pub", pub_topic, data)
-end)
-sys.subscribe("mqtt_payload", function(topic, payload)
-    log.info("uart", "uart发送数据长度", #payload)
-    uart.write(1, payload)
-end)
-
-sys.taskInit(function ()
-    while true do
-        sys.wait(3000)
-        log.info("lua", rtos.meminfo())
-        log.info("sys", rtos.meminfo("sys"))
-    end
 end)
