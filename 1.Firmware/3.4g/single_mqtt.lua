0302d7e327d34ad6ed5b6c505fd743d11d0bd3a5
ErBW_s <erbws@foxmail.com>
2025-04-03 22:58:48 +0800

    feat: parse uart message

--- 1.Firmware/3.4g/single_mqtt.lua
+++ 1.Firmware/3.4g/single_mqtt.lua
@@ -1,23 +1,19 @@
--- 自动低功耗, 轻休眠模式
--- Air780E支持uart唤醒和网络数据下发唤醒, 但需要断开USB,或者pm.power(pm.USB, false) 但这样也看不到日志了
+-- Uncomment to enable power saving mode.
 -- pm.request(pm.LIGHT)
 
---根据自己的服务器修改以下参数
 local mqtt_host = "0c5e592236.st1.iotda-device.cn-east-3.myhuaweicloud.com"
 local mqtt_port = 8883
-local mqtt_isssl = false
-local client_id = "abc"
-local user_name = "user"
-local password = ""
+local mqtt_isssl = true
+local client_id = "67ea52cbc957870e570e769f_LoRaGateway_0_0_2025040311"
+local user_name = "67ea52cbc957870e570e769f_LoRaGateway"
+local password = "password"
 
-local pub_topic = "/luatos/pub/" .. (mcu.unique_id():toHex())
-local sub_topic = "/luatos/sub/" .. (mcu.unique_id():toHex())
--- local topic2 = "/luatos/2"
--- local topic3 = "/luatos/3"
+local pub_topic = "$oc/devices/" .. user_name .. "/sys/properties/report"
+local sub_topic = ""
 
 local mqttc = nil
 
--- 统一联网函数
+-- Try to connect to 4G network.
 sys.taskInit(function()
     local device_id = mcu.unique_id():toHex()
     device_id = mobile.imei()
@@ -25,30 +21,17 @@ sys.taskInit(function()
     sys.publish("net_ready", device_id)
 end)
 
+-- MQTT.
 sys.taskInit(function()
-    -- 等待联网
+    -- Wait for 4G to be connected.
     local ret, device_id = sys.waitUntil("net_ready")
-    -- 下面的是mqtt的参数均可自行修改
-    client_id = device_id
-    pub_topic = "/luatos/pub/" .. device_id
-    sub_topic = "/luatos/sub/" .. device_id
 
-    -- 打印一下上报(pub)和下发(sub)的topic名称
-    -- 上报: 设备 ---> 服务器
-    -- 下发: 设备 <--- 服务器
-    -- 可使用mqtt.x等客户端进行调试
     log.info("mqtt", "pub", pub_topic)
-    log.info("mqtt", "sub", sub_topic)
-
-    -------------------------------------
-    -------- MQTT 演示代码 --------------
-    -------------------------------------
+    -- log.info("mqtt", "sub", sub_topic)
 
     mqttc = mqtt.create(nil, mqtt_host, mqtt_port, mqtt_isssl)
-
-    mqttc:auth(client_id,user_name,password) -- client_id必填,其余选填
-    -- mqttc:keepalive(240) -- 默认值240s
-    mqttc:autoreconn(true, 3000) -- 自动重连机制
+    mqttc:auth(client_id,user_name,password)
+    mqttc:autoreconn(true, 3000)
 
     mqttc:on(function(mqtt_client, event, data, payload)
         -- 用户自定义代码
@@ -87,32 +70,45 @@ sys.taskInit(function()
     mqttc = nil
 end)
 
--- 这里演示在另一个task里上报数据, 会定时上报数据,不需要就注释掉
-sys.taskInit(function()
-    sys.wait(3000)
-	local data = "123,"
-	local qos = 1 -- QOS0不带puback, QOS1是带puback的
-    while true do
-        sys.wait(3000)
-        if mqttc and mqttc:ready() then
-            local pkgid = mqttc:publish(pub_topic, data .. os.date(), qos)
-            -- local pkgid = mqttc:publish(topic2, data, qos)
-            -- local pkgid = mqttc:publish(topic3, data, qos)
-        end
-    end
-end)
-
+-- Receive sensor date from uart2.
 local uart_id = 2
 uart.setup(uart_id, 115200)
 uart.on(uart_id, "receive", function(id, len)
-    local data = ""
-    while true do
-        local tmp = uart.read(uart_id)
-        if not tmp or #tmp == 0 then
-            break
+    local data = uart.read(uart_id)
+	local qos = 1
+
+    -- Decode.
+    local temp = tonumber(data:match("temp:%s*([%d%.]+)"))
+    local humi = tonumber(data:match("humi:%s*([%d%.]+)"))
+    local smoke = tonumber(data:match("smoke:%s*([%d%.]+)"))
+
+    -- Publish.
+    if mqttc and mqttc:ready() then
+        if temp and temp ~= 0 then
+            local json_str = string.format(
+                '{"services":[{"service_id":"sensors","properties":{"temperature":%.2f}}]}',
+                temp
+            )
+            log.info("uart", "temp:", json_str)
+            mqttc:publish(pub_topic, json_str, qos)
         end
-        data = data .. tmp
+
+        if humi and humi ~= 0 then
+            local json_str = string.format(
+                '{"services":[{"service_id":"sensors","properties":{"humidity":%.2f}}]}',
+                humi
+            )
+            log.info("uart", "humi:", json_str)
+            mqttc:publish(pub_topic, json_str, qos)
+        end
+
+        if smoke and smoke ~= 0 then
+            local json_str = string.format(
+                '{"services":[{"service_id":"sensors","properties":{"smoke":%.2f}}]}',
+                smoke
+            )
+            log.info("uart", "temp:", json_str)
+            mqttc:publish(pub_topic, json_str, qos)
+        end
     end
-    log.info("uart", "\r\n", data)
-    sys.publish("mqtt_pub", pub_topic, data)
 end)
