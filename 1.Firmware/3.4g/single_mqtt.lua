86805e1405852c612570424ca89e920a72e7eec0
ErBW_s <erbws@foxmail.com>
2025-04-04 00:36:42 +0800

    feat: basic lua code without test

--- 1.Firmware/3.4g/single_mqtt.lua
+++ 1.Firmware/3.4g/single_mqtt.lua
@@ -15,56 +15,37 @@ local mqttc = nil
 
 -- Try to connect to 4G network.
 sys.taskInit(function()
-    local device_id = mcu.unique_id():toHex()
-    device_id = mobile.imei()
+    local device_id = mobile.imei()
     sys.waitUntil("IP_READY")
     sys.publish("net_ready", device_id)
 end)
 
--- MQTT.
+-- MQTT thread.
 sys.taskInit(function()
     -- Wait for 4G to be connected.
     local ret, device_id = sys.waitUntil("net_ready")
 
+    -- Print publish topic.
     log.info("mqtt", "pub", pub_topic)
-    -- log.info("mqtt", "sub", sub_topic)
 
     mqttc = mqtt.create(nil, mqtt_host, mqtt_port, mqtt_isssl)
-    mqttc:auth(client_id,user_name,password)
+    mqttc:auth(client_id, user_name, password)
     mqttc:autoreconn(true, 3000)
 
     mqttc:on(function(mqtt_client, event, data, payload)
-        -- 用户自定义代码
+        -- Print event.
         log.info("mqtt", "event", event, mqtt_client, data, payload)
         if event == "conack" then
-            -- 联上了
             sys.publish("mqtt_conack")
-            mqtt_client:subscribe(sub_topic)--单主题订阅
-            -- mqtt_client:subscribe({[topic1]=1,[topic2]=1,[topic3]=1})--多主题订阅
-        elseif event == "recv" then
-            log.info("mqtt", "downlink", "topic", data, "payload", payload)
-            sys.publish("mqtt_payload", data, payload)
-        elseif event == "sent" then
-            -- log.info("mqtt", "sent", "pkgid", data)
-        -- elseif event == "disconnect" then
-            -- 非自动重连时,按需重启mqttc
-            -- mqtt_client:connect()
         end
     end)
 
-    -- mqttc自动处理重连, 除非自行关闭
+    -- MQTT auto reconnect.
     mqttc:connect()
 	sys.waitUntil("mqtt_conack")
     while true do
-        -- 演示等待其他task发送过来的上报信息
-        local ret, topic, data, qos = sys.waitUntil("mqtt_pub", 300000)
-        if ret then
-            -- 提供关闭本while循环的途径, 不需要可以注释掉
-            if topic == "close" then break end
-            mqttc:publish(topic, data, qos)
-        end
-        -- 如果没有其他task上报, 可以写个空等待
-        --sys.wait(60000000)
+        -- Just wait.
+        sys.wait(60000000)
     end
     mqttc:close()
     mqttc = nil
@@ -74,7 +55,7 @@ end)
 local uart_id = 2
 uart.setup(uart_id, 115200)
 uart.on(uart_id, "receive", function(id, len)
-    local data = uart.read(uart_id)
+    local data = uart.read(id)
 	local qos = 1
 
     -- Decode.
