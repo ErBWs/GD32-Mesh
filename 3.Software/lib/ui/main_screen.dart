eeed3f7238017b247a1a7e6455387f5ec55aeff0
ErBW_s <erbws@foxmail.com>
2025-04-12 19:57:04 +0800

    feat: refresh token after 24h

--- 3.Software/lib/ui/main_screen.dart
+++ 3.Software/lib/ui/main_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:meshw/network/iot.dart';
@@ -21,106 +22,136 @@ class _MainScreenState extends State<MainScreen> {
   late UserCredential credential;
 
   bool passwordVisible = false;
+  bool isLoggedIn = false;
 
-  Widget _buildLoginForm() {
-    return Form(
-      key: formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: userNameController,
-              decoration: InputDecoration(
-                labelText: '用户名',
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    userNameController.clear();
-                  },
-                  icon: const Icon(Icons.cancel_outlined),
+  void _refreshToken({
+    required String userName,
+    required String password,
+  }) async {
+    try {
+      token = await iot.getAuthToken(
+        userName: userName,
+        password: password,
+      );
+      credential.userName = userName;
+      credential.password = password;
+      if (token.token.isNotEmpty) {
+        hiveInstance.userToken.put('token', token);
+        hiveInstance.userCredential.put('credential', credential);
+        setState(() {
+          isLoggedIn = true;
+        });
+      }
+    } on DioException catch (e) {
+      String message = "";
+      setState(() {
+        isLoggedIn = false;
+      });
+      if (mounted) {
+        switch (e.response?.statusCode) {
+          case 401:
+            message = "HTTP Error(401): 登录失败，请检查用户名和密码";
+            break;
+          case 403:
+            message = "HTTP Error(403): 登录失败，该用户没有权限";
+            break;
+          case 404:
+            message = "HTTP Error(404): 未找到资源";
+            break;
+          case 500:
+            message = "HTTP Error(500): 内部服务错误";
+            break;
+          case 503:
+            message = "HTTP Error(503): 服务不可用";
+            break;
+          default:
+            message = e.message!;
+        }
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(message)));
+      }
+    }
+  }
+
+  Widget get _buildLoginForm {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 500),
+        child: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: userNameController,
+                  decoration: InputDecoration(
+                    labelText: '用户名',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        userNameController.clear();
+                      },
+                      icon: const Icon(Icons.cancel_outlined),
+                    ),
+                    filled: true,
+                  ),
+                  validator: (value) =>
+                      value?.isEmpty ?? true ? '请输入用户名' : null,
                 ),
-                filled: true,
-              ),
-              validator: (value) => value?.isEmpty ?? true ? '请输入用户名' : null,
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              controller: passwordController,
-              keyboardType: TextInputType.visiblePassword,
-              decoration: InputDecoration(
-                labelText: '密码',
-                suffixIcon: IconButton(
-                  onPressed: () {
-                    setState(() {
-                      passwordVisible = !passwordVisible;
-                    });
-                  },
-                  icon: Icon(passwordVisible
-                      ? Icons.visibility_rounded
-                      : Icons.visibility_off_rounded),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    labelText: '密码',
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          passwordVisible = !passwordVisible;
+                        });
+                      },
+                      icon: Icon(passwordVisible
+                          ? Icons.visibility_rounded
+                          : Icons.visibility_off_rounded),
+                    ),
+                    filled: true,
+                  ),
+                  obscureText: !passwordVisible,
+                  validator: (value) => value?.isEmpty ?? true ? '请输入密码' : null,
                 ),
-                filled: true,
-              ),
-              obscureText: !passwordVisible,
-              validator: (value) => value?.isEmpty ?? true ? '请输入密码' : null,
-            ),
-            const SizedBox(height: 8),
-            FilledButton.tonal(
-              onPressed: () async {
-                if (formKey.currentState!.validate()) {
-                  try {
-                    token = await iot.getAuthToken(
-                      userName: userNameController.text,
-                      password: passwordController.text,
-                    );
-                    credential.userName = userNameController.text;
-                    credential.password = passwordController.text;
-                    if (token.token.isNotEmpty) {
-                      hiveInstance.userToken.put('token', token);
-                      hiveInstance.userCredential.put('credential', credential);
+                const SizedBox(height: 8),
+                FilledButton.tonal(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      _refreshToken(
+                          userName: userNameController.text,
+                          password: passwordController.text);
                     }
-                  } on DioException catch (e) {
-                    String message = "";
-                    if (mounted) {
-                      switch (e.response?.statusCode) {
-                        case 401:
-                          message = "HTTP Error(401): 登录失败，请检查用户名和密码";
-                          break;
-                        case 403:
-                          message = "HTTP Error(403): 登录失败，该用户没有权限";
-                          break;
-                        case 404:
-                          message = "HTTP Error(404): 未找到资源";
-                          break;
-                        case 500:
-                          message = "HTTP Error(500): 内部服务错误";
-                          break;
-                        case 503:
-                          message = "HTTP Error(503): 服务不可用";
-                          break;
-                        default:
-                          message = e.message!;
-                      }
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(SnackBar(content: Text(message)));
-                    }
-                  }
-                }
-              },
-              style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.resolveWith((_) {
-                  return Theme.of(context).colorScheme.primary.withAlpha(20);
-                }),
-              ),
-              child: Text(
-                "登录",
-                style: TextStyle(color: Theme.of(context).colorScheme.primary),
-              ),
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.resolveWith((_) {
+                      return Theme.of(context)
+                          .colorScheme
+                          .primary
+                          .withAlpha(20);
+                    }),
+                  ),
+                  child: Text(
+                    "登录",
+                    style:
+                        TextStyle(color: Theme.of(context).colorScheme.primary),
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget get _buildItem {
+    return Center(child: Text("登录成功"));
   }
 
   @override
@@ -142,16 +173,30 @@ class _MainScreenState extends State<MainScreen> {
     )!;
     userNameController.text = credential.userName;
     passwordController.text = credential.password;
+    if (token.token.isNotEmpty) {
+      isLoggedIn = true;
+      if (DateTime.now()
+              .difference(DateTime.fromMillisecondsSinceEpoch(token.time))
+              .inHours >=
+          24) {
+        if (credential.userName.isNotEmpty && credential.password.isNotEmpty) {
+          _refreshToken(
+              userName: credential.userName, password: credential.password);
+        }
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-        title: const Text("Mesh-Watcher"),
-      ),
-      body: _buildLoginForm(),
+      appBar: (Platform.isMacOS || Platform.isWindows || Platform.isLinux)
+          ? AppBar(
+              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+              title: const Text("Mesh-Watcher"),
+            )
+          : null,
+      body: isLoggedIn ? _buildItem : _buildLoginForm,
     );
   }
 }
